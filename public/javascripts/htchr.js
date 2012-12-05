6184c36d53997c55dd73c73272477b49f2cef99c
Matt Schallert <mattschallert@gmail.com>
2012-12-04 19:30:16 -0500

    Keep showing private events in the news feed
    For now, will change when we get to permissions stuff

--- public/javascripts/htchr.js
+++ public/javascripts/htchr.js
@@ -142,7 +142,7 @@ function feedPageInit () {
     $.getJSON('/events/feed.json', function (data) {
         _.each(data, function (hEvent) {
             // used `hEvent` instead of `event` because `event` is a javascript reserved keyword
-            if (hEvent.isPrivate === false && ich.eventItem){
+            if (ich.eventItem){
                 var eventLi = ich.eventItem({
                     user_name: hEvent.ownerName,
                     event_name: hEvent.name,
